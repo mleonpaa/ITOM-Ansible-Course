1783493adfa0029886c32822424141e9bc6e3111
Marcos León Paavilainen <marcos.leon098@gmail.com>
2025-05-05 14:23:08 +0200

    Update config.sh

--- vagrant-project/config.sh
+++ vagrant-project/config.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get install build-essential linux-headers-$(uname -r) -y
-sudo apt-get install virtualbox-guest-dkms -y
-
-sleep 5
-
 sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sudo service ssh restart
 
